540a6377766acfd1fe8580b3874aa9a8f407a77d
InmaTheKeeper <InmaTheKeeper@yandex.ru>
2024-09-12 18:52:59 +0300

    add basic mouse tracking

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtGui import QPainter, QColor
 
 
 class MyWidget(QtWidgets.QWidget):
@@ -10,8 +11,7 @@ class MyWidget(QtWidgets.QWidget):
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
         self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
@@ -19,9 +19,24 @@ class MyWidget(QtWidgets.QWidget):
 
         self.button.clicked.connect(self.magic)
 
+        # Таймер для обновления положения мыши
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.track_mouse_position)
+        self.timer.start(16)  # Обновление каждые 16 мс (примерно 60 кадров в секунду)
+
     @QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
+
+    def track_mouse_position(self):
+        # Получаем глобальные координаты мыши
+        pos = QtGui.QCursor.pos()
+
+        # Преобразуем глобальные координаты в координаты внутри окна
+        local_pos = self.mapFromGlobal(pos)
+
+        # Выводим координаты в консоль
+        print(f"Координаты мыши: X={local_pos.x()}, Y={local_pos.y()}")
 
 
 if __name__ == "__main__":
