4bddd288cb343c8760efc4dd2e8360fc2d77b96f
admin <popasena2@ya.ru>
2024-09-12 21:44:45 +0300

    Added collision with borders and target

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ import logging
 import datetime
 import os
 
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtWidgets, QtGui
+from PySide6.QtCore import QPoint
 from PySide6.QtGui import QPainter
 
-from map import Map, JsonLoader
+from map import Map, JsonLoader, Point
+from map.map_ import DIMENSION
 from map.painter import MapPainter
 
 LOG_DIR = "logs"
@@ -17,7 +19,7 @@ if not os.path.exists(LOG_DIR):
 log_filename = os.path.join(LOG_DIR, f'mouse_position_{current_time}.log')
 
 logging.basicConfig(
-    filename= log_filename,  # Лог будет сохраняться в файл "mouse_position.log"
+    filename=log_filename,  # Лог будет сохраняться в файл "mouse_position.log"
     level=logging.INFO,  # Устанавливаем уровень логирования
     format='%(asctime)s - %(message)s',  # Формат сообщения с указанием времени
     datefmt='%Y-%m-%d %H:%M:%S',  # Формат времени
@@ -25,46 +27,71 @@ logging.basicConfig(
 )
 
 
-class MyWidget(QtWidgets.QWidget):
+class MainWindow(QtWidgets.QWidget):
     _map: Map
     _painter: QPainter
     _map_painter: MapPainter
+    _last_cursor_pos: QPoint = None
 
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("hmi")
+
         self._map = JsonLoader("map.json").load()
         self._painter = QPainter(self)
         self._map_painter = MapPainter(self._map, (self.width(), self.height()))
-        
-        self.timer = QtCore.QTimer(self)
-        self.timer.timeout.connect(self.track_mouse_position)
-        self.timer.start(16)  # Обновление каждые 16 мс (примерно 60 кадров в секунду)
+
+        self.setMouseTracking(True)
+
+    @staticmethod
+    def _log_mouse_position(pos: QPoint):
+        x = pos.x()
+        y = pos.y()
+        print(f"Координаты мыши: X={x}, Y={y}")
+        logging.info(f"Координаты мыши: X={x}, Y={y}")
+
+    def _cursor_return(self, pos: QPoint):
+        width, height = self.width(), self.height()
+        point = Point(
+            int(pos.x() * DIMENSION / width),
+            int(pos.y() * DIMENSION / height)
+        )
+        if self._map.collide_with_borders(point) and self._last_cursor_pos:
+            self.cursor().setPos(self.mapToGlobal(self._last_cursor_pos))
+        elif self._map.collide_with_target(point):
+            logging.info(f"Цель достигнута x={pos.x()} y={pos.y()}")
+            dlg = QtWidgets.QMessageBox(self)
+            dlg.setWindowTitle("Конец")
+            dlg.setText("Цель достигнута")
+            dlg.resize(250, 50)
+            dlg.exec()
+            qpoint = QPoint()
+            qpoint.setX(0)
+            qpoint.setY(0)
+            self.cursor().setPos(self.mapToGlobal(qpoint))
+        else:
+            self._last_cursor_pos = pos
+
+    def mouseMoveEvent(self, event):
+        pos = QtGui.QCursor.pos()
+        # Преобразуем глобальные координаты в координаты внутри окна
+        local_pos = self.mapFromGlobal(pos)
+        self._log_mouse_position(local_pos)
+        self._cursor_return(local_pos)
+
+    def resizeEvent(self, event):
+        self._map_painter.window_size = (self.width(), self.height())
 
     def paintEvent(self, event):
         self._painter.begin(self)
-        self._map_painter.window_size = (self.width(), self.height())
         self._map_painter.paint_objects(self._painter)
         self._painter.end()
-
-    def track_mouse_position(self):
-        # Получаем глобальные координаты мыши
-        pos = QtGui.QCursor.pos()
-
-        # Преобразуем глобальные координаты в координаты внутри окна
-        local_pos = self.mapFromGlobal(pos)
-        x = local_pos.x()
-        y = local_pos.y()
-        # логируем положение мыши
-        if (self.width() > x >= 0) and (0 <= y < self.height()):
-            logging.info(f"Координаты мыши: X={x}, Y={y}")
-            # Выводим координаты в консоль
-            print(f"Координаты мыши: X={x}, Y={y}")
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
+    widget = MainWindow()
     widget.resize(800, 600)
     widget.show()
 
--- app/map/map_.py
+++ app/map/map_.py
@@ -1,5 +1,6 @@
 import random
 
+from abc import ABC, abstractmethod
 from itertools import product
 from typing import List, Tuple
 from dataclasses import dataclass
@@ -21,8 +22,14 @@ class Point:
         return self.x, self.y
 
 
+class AbstractContainsMixin(ABC):
+    @abstractmethod
+    def contains(self, point: Point) -> bool:
+        raise NotImplementedError
+
+
 @dataclass
-class Rect:
+class Rect(AbstractContainsMixin):
     corner: Point  # Левый верхний угол
     width: int
     height: int
@@ -32,25 +39,36 @@ class Rect:
             raise ValueError("Rect's width and height must be greater than zero.")
 
     def contains(self, point: Point) -> bool:
-        x, y = self.corner.x, self.corner.y
-        return x <= point.x < x + self.width and y <= point.y <= y + self.height
+        x, y = self.corner.to_tuple()
+        return x <= point.x < x + self.width and y <= point.y < y + self.height
 
 
 @dataclass
-class Circle:
+class Circle(AbstractContainsMixin):
     pos: Point
     is_target: bool
     radius: int = 10
+
+    def contains(self, point: Point) -> bool:
+        cx, cy = self.pos.to_tuple()
+        x, y = point.to_tuple()
+        dist = ((cx - x) ** 2 + (cy - y) ** 2) ** (1 / 2)
+        return dist <= self.radius
 
 
 class Map:
     OBJECTS_COUNT = 15
     _borders: List[Rect]
     _targets: List[Circle]
+    _target: Circle
 
     def __init__(self, borders: List[Rect], targets: List[Circle] = None):
         self._borders = borders
         self._targets = targets or self._generate_targets()
+        try:
+            self._target = list(filter(lambda x: x.is_target, self._targets))[0]
+        except IndexError:
+            raise MapException("Target does not exist")
         self._validate_objects(self._targets)
 
     def _generate_targets(self) -> List[Circle]:
@@ -71,6 +89,12 @@ class Map:
         for rect in self._borders:
             if any(rect.contains(obj.pos) for obj in objects):
                 raise MapException("Obj in border")
+
+    def collide_with_borders(self, pos: Point) -> bool:
+        return any(border.contains(pos) for border in self._borders)
+
+    def collide_with_target(self, pos: Point) -> bool:
+        return self._target.contains(pos)
 
     @property
     def borders(self) -> List[Rect]:
